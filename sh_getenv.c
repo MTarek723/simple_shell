19d6364901382014a2c7e9b86e7e88aa6ed85912
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-10-17 11:30:08 -0700

    L

---
+++ sh_getenv.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* sh_getenv - gets the value of an environ variable
+* @call: enviromental variable name
+*
+* Return: the value
+*/
+char *sh_getenv(const char *call)
+{
+int i;
+for (i = 0; environ[i] != NULL; i++)
+{
+char *envr = sh_strdup(environ[i]);
+char *tkn = strtok(envr, "=");
+if (sh_strcmp(tkn, call) == 0)
+{
+char *val = strtok(NULL, "=");
+return (val);
+}
+free(envr);
+}
+return (NULL);
+}

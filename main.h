ed93ca45393aa8319b04a6cda39386e7c1a47951
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-10-17 11:21:35 -0700

    L

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+#define EXIT "exit\n"
+#define ENV "env\n"
+#define BUFF_SIZE 1024
+#define DELIM " \n"
+
+char *locate(char *command);
+void excut(char **argv);
+char **split(char *buff, int x);
+int sh_strcmp(const char *str1, const char *str2);
+int sh_strlen(char *str);
+char *sh_strcat(char *dst, char *sorc);
+void sh_puts(char *s);
+int sh_putchar(char c);
+char *sh_strdup(const char *s);
+char *sh_strcpy(char *dst, char *sorc);
+char *sh_getenv(const char *call);
+#endif

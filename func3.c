c56be0a563e6f7859ac3a2be5e8d0e93ad50e8bf
Abdallah 616 <yassinabdou59@gmail.com>
2023-10-17 11:24:39 -0700

    simple shell

---
+++ func3.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * sh_strdup - duplicates a string
+ * @s: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
+char *sh_strdup(const char *s)
+{
+	int len = 0;
+	char *fin;
+
+	if (s == NULL)
+		return (NULL);
+	while (*s++)
+		len++;
+	fin = malloc(sizeof(char) * (len + 1));
+	if (!fin)
+		return (NULL);
+	for (len++; len--;)
+		fin[len] = *--s;
+	return (fin);
+}
+
+/**
+ * sh_strcpy - copies a string
+ * @dst: the destination
+ * @sorc: the source
+ *
+ * Return: pointer to destination
+ */
+char *sh_strcpy(char *dst, char *sorc)
+{
+	int i = 0;
+
+	if (dst == sorc || sorc == 0)
+		return (dst);
+	while (sorc[i] != '\0')
+	{
+		dst[i] = sorc[i];
+		i++;
+	}
+	dst[i] = 0;
+	return (dst);
+}

71d1d01859f16359e04bdfec0d9d0bcce388f6d5
Abdallah 616 <yassinabdou59@gmail.com>
2023-10-17 15:17:36 -0700

    test

--- main.c
+++ main.c
@@ -1,24 +1,23 @@
 #include "main.h"
 /**
  * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * @argc: arg count
+ * @argv: arg vector
  *
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char **argv)
 {
-
 char *buff;
 size_t len = 0;
-int x;
-int k;
+int x, k;
+
 (void) argc;
 while (1)
 {
 if (isatty(STDIN_FILENO))
 {
-sh_puts("($) "); 
+sh_puts("($) ");
 }
 x = getline(&buff, &len, stdin);
 if (x == -1)
@@ -27,9 +26,7 @@ sh_puts("\n");
 return (-1);
 }
 if (sh_strcmp(buff, EXIT) == 0)
-{
 break;
-}
 else if (sh_strcmp(buff, ENV) == 0)
 {
 for (k = 0; environ[k]; k++)
@@ -41,15 +38,11 @@ continue;
 }
 argv = split(buff, x);
 if (argv[0] == NULL)
-{
 continue;
-}
 excut(argv);
 fflush(stdout);
 for (k = 0; argv[k]; k++)
-{
 free(argv[k]);
-}
 }
 free(argv);
 free(buff);

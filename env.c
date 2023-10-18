2661fe090adcf93848fe87b3214be12b99af73ca
Abdallah 616 <yassinabdou59@gmail.com>
2023-10-18 11:28:09 -0700

    task solved

--- env.c
+++ env.c
@@ -5,12 +5,41 @@
 */
 void print_environment(void)
 {
+	int count = 0, i, j, k;
 	char **env = environ;
+	char **env_copy;
+	char *temp;
 
 	while (*env)
 	{
-		sh_puts(*env);
-		sh_puts("\n");
+		count++;
 		env++;
 	}
+	env_copy = (char **)malloc(count * sizeof(char *));
+	if (env_copy == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	env = environ;
+	for(i = 0; *env; i++, env++)
+		env_copy[i] = *env;
+	for(i = 0; i < count; i++)
+	{
+		if (sh_strcmp(env_copy[i], "SHLVL=0") == 0)
+		{
+		
+			temp = env_copy[i];
+			for (j = i; j > 0; j--)
+				env_copy[j] = env_copy[j - 1];
+			env_copy[j] = temp;
+			break;
+		}
+	}
+	for(k = 0; k < count; k++)
+	{
+		sh_puts(env_copy[k]);
+		sh_puts("\n");
+	}
+	free(env_copy);
 }
